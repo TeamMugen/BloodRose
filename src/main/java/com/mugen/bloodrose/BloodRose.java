5f1746053c603f4ce9ebcd48d04d4335354837a7
Ksan1333 <41532872+Ksan1333@users.noreply.github.com>
2024-11-05 00:25:11 +0900

    First commit

---
+++ src/main/java/com/mugen/bloodrose/BloodRose.java
@@ -0,0 +1,17 @@
+package com.mugen.bloodrose;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BloodRose extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
